1ed2b7b9b44ff1e5871d4860b4bdd6a3e95b7c3d
adithdotv <adithadiabd17@gmail.com>
2026-03-18 15:45:38 +0530

    checking(2)

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import AdminScanner from "./pages/AdminScanner";
 const App = () => {
   return (
     <Routes>
-      //Admin route
+      {/* Admin route */}
       <Route path="/admin/scanner" element={<AdminScanner />} />
       <Route path="*" element={
         <div className="min-h-screen bg-black">
